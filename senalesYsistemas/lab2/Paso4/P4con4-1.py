d182949389e329503de618f3ed96c30a5245bf44
pc-miki <miguelpereira3005@gmail.com>
2025-10-14 16:52:25 -0300

    ultimos cambios a los nombres de las variables, ya que algunas estaban incorrectas

--- senalesYsistemas/lab2/Paso4/P4con4-1.py
+++ senalesYsistemas/lab2/Paso4/P4con4-1.py
@@ -17,19 +17,19 @@ sierra_n = (sierra+1)/2                  # [0,1]  <<— igual que en Paso 1
 u = lambda x: np.where(x >= 0, 1, 0)
 
 
-x_exp_dec = np.exp(t) * (u(t) - u(t - 1))   # e^{t}[u(t)-u(t-1)]
+x_exp_cre = np.exp(t) * (u(t) - u(t - 1))   # e^{t}[u(t)-u(t-1)]
 
 #Escalamiento de la respuesta al impulso
 A = 10
-x_exp_dec = A * x_exp_dec  # este seria como un "h"
+x_exp_cre = A * x_exp_cre  # este seria como un "h"
 
 # === Convolución continua aproximada ===
-y = np.convolve(sierra_n, x_exp_dec, mode='full') * dt
+y = np.convolve(sierra_n, x_exp_cre, mode='full') * dt
 t_y = np.arange(t[0]+t[0], t[-1]+t[-1]+dt, dt)[:len(y)]
 
 #  Gráficos generales
 plt.figure(figsize=(10,7))
 plt.subplot(3,1,1); plt.plot(t, sierra_n);      plt.title('x(t): sierra periodica (0–1)');       plt.grid(True)
-plt.subplot(3,1,2); plt.plot(t, x_exp_dec); plt.title('h(t): e^{-t}[u(t)-u(t-1)]');            plt.grid(True)
+plt.subplot(3,1,2); plt.plot(t, x_exp_cre); plt.title('h(t): e^{-t}[u(t)-u(t-1)]');            plt.grid(True)
 plt.subplot(3,1,3); plt.plot(t_y, y);       plt.title('y(t) = x * h');    plt.grid(True)
 plt.tight_layout(); plt.show()
